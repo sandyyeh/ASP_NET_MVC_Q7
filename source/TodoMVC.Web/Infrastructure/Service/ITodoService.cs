cedcbd510aec7d4cc3266f675f03a8551453cd80
sandyyeh <sandyy9996@gmail.com>
2019-07-03 16:46:47 +0800

    Service 及 Ajax-Idex.cshtml 調整 1. service 加入 interface (ITodoService) 2. button click 事件寫法調整 3. style 改用 css

---
+++ source/TodoMVC.Web/Infrastructure/Service/ITodoService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using TodoMVC.Web.Models;
+
+namespace TodoMVC.Web.Service
+{
+    public interface ITodoService
+    {
+        void Create(TodoModel todoModel);
+        void Update(int id);
+        void Delete(int id);
+        void Clear();
+        IEnumerable<TodoModel> GetAll(bool? status);
+
+    }
+}
--- source/TodoMVC.Web/Infrastructure/Service/TodoService.cs
+++ source/TodoMVC.Web/Infrastructure/Service/TodoService.cs
@@ -1,17 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using TodoMVC.Web.Infrastructure.Interface;
 using TodoMVC.Web.Infrastructure.Repository;
 using TodoMVC.Web.Models;
 
+
+
 namespace TodoMVC.Web.Service
 {
-    public class TodoService
+    public class TodoService:ITodoService
     {
 
-        private ITodoRepository _todoRepository;
+        private readonly ITodoRepository _todoRepository;
 
         public TodoService()
         {
