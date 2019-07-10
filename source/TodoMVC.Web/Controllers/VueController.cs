caa5109e150009e55789b4037c8f2193953c7321
sandyyeh <sandyy9996@gmail.com>
2019-07-10 18:18:07 +0800

    Vue 實作，細節待完成

--- source/TodoMVC.Web/Controllers/VueController.cs
+++ source/TodoMVC.Web/Controllers/VueController.cs
@@ -1,17 +1,97 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TodoMVC.Web.Models;
+using TodoMVC.Web.Service;
 
 namespace TodoMVC.Web.Controllers
 {
     public class VueController : Controller
     {
-        // GET: Vue
+
+        private DatabaseEntities1 _db = new DatabaseEntities1();
+        private TodoService _todoService;
+
+
+        public VueController()
+        {
+            _todoService = new TodoService();
+        }
+
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult List(bool? status)
+        {
+            string json = "";
+            var list = _db.TodoModel.OrderBy(o => o.Id).ToList();
+
+            if (status == true)
+            {
+                list = _todoService.GetAll(status).ToList();
+            }
+            else if (status == false)
+            {
+                list = _todoService.GetAll(status).ToList();
+            }
+
+
+            var setting = new JsonSerializerSettings
+            {
+                ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver()
+            };
+            json = JsonConvert.SerializeObject(list, Formatting.None, setting);
+
+            return Json(json, JsonRequestBehavior.AllowGet);
+
+        }
+
+        [HttpPost]
+        public JsonResult Create(TodoModel todoModel)
+        {
+
+            _todoService.Create(todoModel);
+            string json = JsonConvert.SerializeObject(todoModel);
+
+            return Json(json, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult Update(int id)
+        {
+            _todoService.Update(id);
+            return Json(new { success = true });
+
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            _todoService.Delete(id);
+            return Json(new { success = true });
+        }
+
+        [HttpPost]
+        public ActionResult Clear()
+        {
+            _todoService.Clear();
+            return Json(new { success = true });
+        }
+
+        //可有可無，此為保險起見，釋放資源用途
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
---
+++ source/TodoMVC.Web/Scripts/Vue/app.js
@@ -0,0 +1,100 @@
+﻿window.onload = function () {
+  
+    var app = new Vue({
+        el: "#app",
+        data: {
+            list: []
+            ,
+            addItem:
+            {
+                status: false          
+            },
+            
+     
+     
+        },
+        created: function () {
+
+            this.todoList();
+        },
+      
+        methods: {
+            todoList: function (status) {
+                var me = this;
+                $.ajax({
+                    url: '/Vue/List',
+                    type: 'get',
+                    data: { status: status },
+                    success: function (data) {
+                        data = JSON.parse(data);                   
+                        me.list = data;                
+                        
+                    }
+                });
+            },
+
+
+            create: function (inputValue) {
+                var me = this;
+                $.ajax({
+                    url: '/Vue/Create',
+                    data: { content: this.inputValue },
+                    type: 'post',
+                    dataType: 'json',
+                    success: function (data) {
+                        data = JSON.parse(data);
+                        me.list = data;
+                        me.todoList();
+                    }
+
+                });
+
+                inputValue = '';
+
+            },
+
+
+            clear: function () {
+                var me = this;
+                $.ajax({
+                    url: '/Vue/Clear',
+                    type: 'post',
+                    success: function () {
+                        me.todoList();
+                    }
+                });
+            },
+
+            delete1: function (deleteId, status) {
+                var me = this;
+                $.ajax({
+                    url: '/Vue/Delete',
+                    data: { id: deleteId },
+                    type: 'post',
+                    dataType: 'json',
+                    success: function (data) {
+                        me.todoList(status);
+                    }
+                });
+            },
+
+            changeStatus: function (changeId, status) {
+                var me = this;
+                $.ajax({
+                    url: '/Vue/Update',
+                    data: { id: changeId },
+                    type: 'post',
+                    dataType: 'json',
+                    success: function (data) {
+                        me.todoList(status);
+                    }
+                });
+            },
+           
+
+
+        }
+
+
+    })
+}
